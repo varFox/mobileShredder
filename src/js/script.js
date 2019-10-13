0b9f9ec4bdd0d3f294a9c78ad4261d5626aa38b8
Lyubov <aira_dark@live.ru>
2019-10-13 21:04:43 +0300

    1

---
+++ src/js/script.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const popup = document.querySelector('#popup'),
+    popupPolitic = document.querySelector('#popup-politic'),
+    mobilMenu = document.querySelector('.menu-mobil');
+  document.querySelector('.showPopup').addEventListener('click', () => {
+    popup.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+  });
+  document.querySelector('#showPolitic').addEventListener('click', () => {
+    popupPolitic.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+  });
+  document.querySelectorAll('.popup-bg, .close').forEach(item => {
+    item.addEventListener('click', event => {
+      if (event.target.classList.contains('popup-bg') ||
+        event.target.classList.contains('close')) {
+        popup.style.display = '';
+        popupPolitic.style.display = '';
+        document.body.style.overflow = '';
+      }
+
+    });
+  });
+  const showMobMenu = () => {
+    if (screen.width <= 540) {
+      const imgMenu = document.querySelector('header .main-menu .icon img');
+      imgMenu.src = '/images/моб.меню.png';
+      document.body.addEventListener('click', event => {
+        if ((event.target.classList.contains('close') && event.target.closest('.menu-mobil')) ||
+          !event.target.closest('.menu-mobil') && mobilMenu.classList.contains('active-menu')) {
+          mobilMenu.classList.remove('active-menu');
+        }
+        if (event.target.src && event.target.closest('.main-menu')) {
+          mobilMenu.classList.add('active-menu');
+        }
+      });      
+    }
+  }
+  showMobMenu();
+
+
+
+
+});
